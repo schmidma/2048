e6a0403764231ba27a254aa0561a8360d596e923
Maximilian Schmidt <maximilian@schmidt.so>
2017-01-16 09:51:15 +0100

    + testSDL.c hinzugefügt + main.c um SDL Init erweitert

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SDL2/SDL.h>
 
 #define DIMENSION 4
 
@@ -17,7 +18,7 @@ void spawn_rand_field(int fields[DIMENSION][DIMENSION]) {
     int x,y;
     int free = 0;
     int *freeFields[DIMENSION*DIMENSION];
-    
+
     for (x = 0; x < DIMENSION; x++) {
         for (y = 0; y < DIMENSION; y++) {
             if (fields[x][y] == 0) {
@@ -26,19 +27,19 @@ void spawn_rand_field(int fields[DIMENSION][DIMENSION]) {
             }
         }
     }
-    
+
     if (!free) {
         printf("GAMEOVER!!!!\n");
         exit(EXIT_SUCCESS);
     }
-    
+
     srand((unsigned int)time(NULL));
     *freeFields[rand() % free] = nmbr;
 }
 
 void m_down (int fields[DIMENSION][DIMENSION]) {
     int x,y;
-    
+
     for (y = 0; y < DIMENSION-1; y++) {
         for (x = 0; x < DIMENSION; x++) {
             if (fields[x][y+1] == fields[x][y]) {
@@ -67,17 +68,24 @@ void print_fields(int fields[DIMENSION][DIMENSION]) {
     printf("\n");
 }
 
-int main(int argc, const char * argv[]) {
-    
+int main(int argc, char* args[]) {
+
     //INITIALIZE VARIABLES
     int round = 0;
     int points = 0;
     int fields[DIMENSION][DIMENSION] = {0};
     int c;
-    
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        printf("SDL_Init failed!\n");
+    else
+        printf("SDL_Init was successful!\n");
+
     print_fields(fields);
-    
-    
-    
+    spawn_rand_field(fields);
+    print_fields(fields);
+
+    SDL_Quit();
+
     return 0;
 }
